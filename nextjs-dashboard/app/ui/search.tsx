4c4d71efd202fdcc3c6027388f68848daed697b4
kpcastillo <krn_iveth09@hotmail.com>
2026-01-25 22:46:37 -0800

    Chapter 13 completed

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -9,7 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  function HandleSearch(term: string) {
     const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
 
@@ -32,7 +32,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          handleSearch(e.target.value);
+          HandleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
       />
